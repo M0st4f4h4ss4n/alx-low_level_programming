401d935a92de641e0b202e3c08bcca1f10a2db88
Mostafa Hasan <m.hasan@odak-eg.com>
2022-09-09 12:39:51 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *
+ * Return: 0 Always (Success)
+ */
+
+
+int main(void)
+{
+	int i = 48;
+	int j = 97;
+	/**Declerations End**/
+	for (; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
