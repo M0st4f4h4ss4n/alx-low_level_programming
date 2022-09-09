fcd285e2097f2fa0dbbbaa503bad6aad893fea4f
Mostafa Hasan <m.hasan@odak-eg.com>
2022-09-09 11:39:47 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ *
+ * Return: 0 Always (Success)
+ */
+
+
+int main(void)
+{
+	for (i = 97;i < 122; i++){
+	putchar(i);
+	}
+	putchar("\n")
+	return (0);
+}
