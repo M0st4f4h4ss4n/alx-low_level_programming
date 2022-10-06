a4a0213f3ed205cc2952fe0563ffaa70f3c90474
Mostafa Hasan <m.hasan@odak-eg.com>
2022-10-06 09:31:41 -0700

    Task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,18 +1,18 @@
-#include <stdlib.h>
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * malloc_checked - allocates memory using malloc
- * @b: Unsigned Integer
- * Return: a pointer to the allocated memory if malloc fails exit  98
+ * malloc_checked - allocates memory.
+ * @b: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-
 void *malloc_checked(unsigned int b)
 {
-	char *mem;
+	char *p;
 
-	mem = malloc(b);
-	if (mem == NULL)
+	p = malloc(b);
+	if (p == NULL)
 		exit(98);
-	return (mem);
+	return (p);
 }
--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * string_nconcat - concatenates two strings.
- * @s1: string 1
- * @s2: string 2
- * @n: Number of Bytes
- * Return: pointer shall point to a newly allocated space in memory
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *sout;
@@ -15,12 +17,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
+
 	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
+
 	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
+
 	if (n > ls2)
 		n = ls2;
 
@@ -38,5 +44,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			sout[i] = s2[i - ls1];
 
 	sout[i] = '\0';
+
 	return (sout);
 }
