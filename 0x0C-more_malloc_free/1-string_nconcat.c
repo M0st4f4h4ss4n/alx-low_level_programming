5b1b99b18dad01044e0d17f1a36b746997a4e6d8
Mostafa Hasan <m.hasan@odak-eg.com>
2022-10-06 09:21:16 -0700

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * @n: Number of Bytes
+ * Return: pointer shall point to a newly allocated space in memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p
+	if (p NULL)
+		return (NULL);
+	return (p);
+}
